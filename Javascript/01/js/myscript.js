e2fdd9313a3e051d72d289c4d4b8a255735b8731
tikinatop <yoplamouchedelespace@gmail.com>
2016-10-18 20:02:02 +0400

    Progression updated (up to 130)

---
+++ Javascript/01/js/myscript.js
@@ -0,0 +1,8 @@
+// console.log("Hello");
+
+var fName = prompt("Hello, what's your first name?");
+var lName = prompt("What's your last name?");
+var age = prompt("How old r u?");
+
+console.log("Your full name is ",fName," ",lName," !");
+console.log("Your are ",age," years old");
---
+++ Javascript/02/calculator.js
@@ -0,0 +1,11 @@
+// var myAge = prompt("Enter your age");
+// var d = new Date();
+// var JohnOliver = d.getFullYear();
+// // Gives a flexible solution across the years to come
+// console.log(JohnOliver);
+// var yearOfBirth = JohnOliver - myAge;
+
+
+var myAge = prompt("Enter your age");
+var daysLived = myAge * 365.25;
+console.log("You lived ", daysLived, " days!");
